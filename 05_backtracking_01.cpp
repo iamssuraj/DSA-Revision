8bcc1c65b23a8617a1fec1336b16e9c25f85a29b
Suraj Sanganbhatla <ssdmp123@gmail.com>
2022-05-08 22:02:04 +0530

    Permutations_using_backtracking\

---
+++ 05_backtracking_01.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+string swap(string s, int a, int b)
+{
+    char t = s[a];
+    s[a] = s[b];
+    s[b] = t;
+    return s;
+}
+void permutations(string s, int left, int right)
+{
+    if (left == right)
+    {
+        cout << s << endl;
+    }
+    else
+    {
+        for (int i = left; i <= right; i++)
+        {
+            s = swap(s, left, i);
+            permutations(s, left + 1, right);
+            s = swap(s, left, i);
+        }
+    }
+}
+signed main()
+{
+    string s;
+    cin >> s;
+    permutations(s, 0, s.size() - 1);
+    return 0;
+}
