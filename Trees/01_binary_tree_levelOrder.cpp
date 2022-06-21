0d2ad456a96a4ca335bb75e3a289ef96a4371114
Suraj Sanganbhatla <ssdmp123@gmail.com>
2022-06-21 22:24:15 +0530

    Level Order Traversal

--- Trees/01_binary_tree_levelOrder.cpp
+++ Trees/01_binary_tree_levelOrder.cpp
@@ -78,6 +78,7 @@ signed main()
     // -1          7        6         8
     //-1  -1   -1   -1   -1  -1    -1  -1
 
+    
     // data : 1 3 -1 7 -1 -1 5 6 -1 -1 8 -1 -1
     cout << endl;
     levelOrderTraversal(root);
